9d7748cda79fe876364089c4487fff51e012472b
Michael Penkov <misha.penkov@gmail.com>
2012-02-10 19:37:14 +0900

    Reverting to multiple files in dist.
    Building into a single executable produced a broken executable for some
    reason.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from distutils.core import setup
 import py2exe
 setup(
-    options = { 'py2exe' : { 'bundle_files' : 1 } },
     windows= [
         { 'script' : 'rvyt.py', 'icon_resources' : [(0, 'Reddit.ico')] } ], 
-    data_files=['rvyt.cfg', 'reddit_api.cfg', 'Reddit2.ico' ],
-    zipfile = None )
+    data_files=['rvyt.cfg', 'reddit_api.cfg', 'Reddit2.ico' ])

03f2705b83e93e3bd5b013c7d66bcdad7eb02237
Michael Penkov <misha.penkov@gmail.com>
2012-02-10 19:30:03 +0900

    Build everything into a single executable.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 import py2exe
 setup(
+    options = { 'py2exe' : { 'bundle_files' : 1 } },
     windows= [
         { 'script' : 'rvyt.py', 'icon_resources' : [(0, 'Reddit.ico')] } ], 
-    data_files=['rvyt.cfg', 'reddit_api.cfg', 'Reddit2.ico' ])
+    data_files=['rvyt.cfg', 'reddit_api.cfg', 'Reddit2.ico' ],
+    zipfile = None )
